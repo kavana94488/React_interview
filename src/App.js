ac781707c9e6c30d2f1406046a2de32d20c27dc9
kavana94488 <58367489+kavana94488@users.noreply.github.com>
2023-04-14 18:34:39 +0530

    second update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 
 import Checkbox from './Interview/Checkbox';
+import Deleting from './Interview/Deleting';
 
 function App() {
   return (
     <div className="App">
      <Checkbox/> 
+     <Deleting/>
     </div>
   );
 }
---
+++ src/Interview/Deleting.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import "./checkbox.css"
+
+const Deleting = () => {
+    const data = [
+      {
+        id: 1,
+        name: "kavana",
+        job: "Software Engineer",
+      },
+      {
+        id: 2,
+        name: "pavana",
+        job: "Software Developer",
+      },
+      {
+        id: 3,
+        name: "namana",
+        job: "Deveops Engineer",
+      },
+      {
+        id: 4,
+        name: "gagana",
+        job: "Test Engineer",
+      },
+    ];
+
+    const[change,setChange] = useState(data)
+    const deleteHandler = (ind) =>{
+    //   console.log(ind)
+        let result = change.filter((item,index)=>{
+            console.log(item.id)
+            return index+1 !==ind
+        })
+        setChange(result)
+
+    }
+    console.log(change)
+  return (
+    <div className="checkbox">
+        <h4>Deleting  options</h4>
+      <div className='deletevalue'>
+        {change.map((item, index) => {
+          return (
+            <div>
+              <span>{item.job}</span>
+              <button
+                key={index}
+                onClick={() => {
+                  deleteHandler(index + 1);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Deleting
