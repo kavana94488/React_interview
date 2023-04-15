9e69b732b07e2ca2b7645e14bbf236957ee8ff78
kavana94488 <58367489+kavana94488@users.noreply.github.com>
2023-04-15 18:25:29 +0530

    added try catch block

--- src/Interview/Searchbar.js
+++ src/Interview/Searchbar.js
@@ -4,15 +4,21 @@ import { useEffect } from 'react'
 const Searchbar = () => {
     const[user,setUser] = useState([])
     const[filteredData,setFilteredData] = useState("")
-
+   const[error,setError] = useState(null)
     const fetchHandler = async() =>{
        try{
         const data = await fetch("https://jsonplaceholder.typicode.com/users")
         const response = await data.json()
         setUser(response)
+        if(!data.ok){
+            throw new Error("Check the API")
+        }
        }
        catch(err){
-        throw new err
+        // console.log("wrong address")
+        // throw new err
+        setError(err.message)
+        console.log(err.message)
        }
     }
 
@@ -31,7 +37,7 @@ const Searchbar = () => {
         <h4>Filtering the data</h4>
         <div>
             <input type="text" onChange={searchHandler}/>
-            {
+            { !error &&
            user.filter(val=>{
             if (filteredData=="") return val
             else if(val.name.toLowerCase().includes(filteredData.toLowerCase())) return val
@@ -39,6 +45,7 @@ const Searchbar = () => {
             <p >{item.name}</p>
            ))
             }
+            {error}
         </div>
     </div>
   )
